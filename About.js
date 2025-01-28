43b6ea231b26fe5acb85ed2ba5b40c5e386b0167
aponce93 <alecponce1993@gmail.com>
2025-01-27 21:30:25 -0700

    Create About.js

---
+++ About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>We are a streaming service offering the best in both movies and subscription plans.</p>
+    </div>
+  );
+};
+
+export default About;
